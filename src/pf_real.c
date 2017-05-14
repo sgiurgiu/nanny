d54401cb622f191fc8bb5d59f743d9906c811a6f
Sergiu Giurgiu <sergiu@mail.zergiu.com>
2017-05-14 16:27:47 -0400

    removed a bunch of includes

--- src/pf_real.c
+++ src/pf_real.c
@@ -3,32 +3,16 @@
 #include <sys/types.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/endian.h>
-
-#include <net/if.h>
-#include <netinet/in.h>
-#include <net/pfvar.h>
-#include <strings.h>
-#include <sys/cdefs.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/endian.h>
 #include <net/if.h>
 #include <netinet/in.h>
 #include <net/pfvar.h>
 #include <arpa/inet.h>
-#include <sys/sysctl.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <arpa/inet.h>
-#include <sys/sysctl.h>
 #include <unistd.h>
 
 #include "database.h"
