d7de37d55e124878298788258b2d9bc1e7859b2f
Sergiu Giurgiu <sgiurgiu11@gmail.com>
2018-04-16 20:52:54 -0400

    added another check

--- tests/check_token.c
+++ tests/check_token.c
@@ -44,6 +44,27 @@ START_TEST(test_token_read)
 }
 END_TEST
 
+START_TEST(test_token_read_bearer)
+{
+    char* token = make_test_token();
+    json_error_t error;
+    json_auto_t* token_json = json_loads(token,0,&error);
+    json_t *json_id = json_object_get(token_json,"id");    
+    const char* token_val = json_string_value(json_id);    
+    size_t header_length = json_string_length(json_id) + 8;
+    char* header_val = (char*)malloc( header_length );
+    strncpy(header_val,"Bearer ",7);
+    strncpy(header_val+7,token_val,header_length-7);
+    user* u = extract_user(header_val,200);
+    ck_assert_ptr_nonnull(u);
+    ck_assert_str_eq(u->login,"login");
+    ck_assert_int_eq(u->roles_count,1);
+    ck_assert_str_eq(u->roles[0],"ROLE");
+    free_user(u);
+    free(token);
+    free(header_val);
+}
+END_TEST
 
 START_TEST(test_token_create)
 {
@@ -72,6 +93,7 @@ Suite * tokens_suite(void)
     tcase_add_checked_fixture(tc_core, setup, teardown);
     tcase_add_test(tc_core, test_token_create);
     tcase_add_test(tc_core, test_token_read);
+    tcase_add_test(tc_core, test_token_read_bearer);
     suite_add_tcase(s, tc_core);
 
     return s;
